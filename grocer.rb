5f61fc7eae4fd5b9ddc10dd72a9ed35d23a2855d
Changmin Lim <chang.lim92@gmail.com>
2018-05-31 20:07:27 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,22 @@
 def consolidate_cart(cart)
-  # code here
+  result = {}
+  count = 0 
+  cart.each do |item|
+    if result.include?(item.keys[0])
+      count += 1 
+    else 
+      count = 1 
+    end 
+    item.each do |item, info|
+      info.each do |key, value|
+        result[item] = info 
+      end 
+      result[item][:count] = count 
+    end 
+  end 
+  result
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
@@ -13,3 +29,5 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+
